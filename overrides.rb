a2e3d96e4b1579f4df43f46c903ea65d3d906c31
Thomas Roehr <thomas.roehr@dfki.de>
2017-03-16 12:55:10 +0100

    Fix setup of TYPELIB_PLUGIN_PATH and setting of CXX_FLAGS to prefer local packages

---
+++ overrides.rb
@@ -0,0 +1,42 @@
+# Setup of the TYPELIB_PLUGIN_PATH
+typelib_plugin_path = Autobuild.environment['TYPELIB_PLUGIN_PATH']
+if !typelib_plugin_path.empty?
+    typelib_plugin_path = typelib_plugin_path.first
+    if !File.exists?(typelib_plugin_path)
+        FileUtils.mkdir_p typelib_plugin_path
+    end
+end
+
+release_dir="/opt/rock/#{Autoproj.user_config('debian_release')}"
+Autoproj.env_set 'TYPELIB_PLUGIN_PATH', File.join(release_dir, 'lib','typelib')
+Autoproj.env_add 'TYPELIB_PLUGIN_PATH', typelib_plugin_path
+
+architecture = "#{`gcc -print-multiarch`}".strip
+deb_cxx_flags = []
+deb_cxx_flags << "-I#{Autoproj.root_dir}/install/include"
+deb_cxx_flags << "-L#{Autoproj.root_dir}/install/lib/#{architecture}"
+deb_cxx_flags << "-L#{Autoproj.root_dir}/install/lib"
+
+Autobuild::Package.each do |name, pkg|
+    if pkg.respond_to?(:define)
+        existing_cxx_flags=pkg.defines['CMAKE_CXX_FLAGS']
+        if existing_cxx_flags
+            existing_cxx_flags = existing_cxx_flags.split(" ")
+        else
+            existing_cxx_flags = []
+        end
+        extra_cxx_flags  = Array.new
+
+        deb_cxx_flags.each do |flag|
+            if existing_cxx_flags.include?(flag)
+                existing_cxx_flags.delete(flag)
+            end
+        end
+        cxx_flags = deb_cxx_flags + existing_cxx_flags
+
+        cxx_flags = cxx_flags.join(" ")
+        if !cxx_flags.empty?
+            pkg.define "CMAKE_CXX_FLAGS", cxx_flags
+        end
+    end
+end
