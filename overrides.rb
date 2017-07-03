6472488ca484e1d2fcbbc2b8e7f8b6dfd60dc512
Thomas Roehr <thomas.roehr@dfki.de>
2017-07-03 14:13:16 +0200

    Make source cxx11 is set for all libraries

--- overrides.rb
+++ overrides.rb
@@ -14,3 +14,30 @@ if Autoproj.user_config('DEB_USE')
         Autoproj.env_set 'TYPELIB_PLUGIN_PATH', File.join(release_dir, 'lib','typelib')
     end
 end
+
+deb_cxx_flags = []
+deb_cxx_flags << "-std=c++11"
+
+Autobuild::Package.each do |name, pkg|
+    if pkg.respond_to?(:define)
+        existing_cxx_flags=pkg.defines['CMAKE_CXX_FLAGS']
+        if existing_cxx_flags
+            existing_cxx_flags = existing_cxx_flags.split(" ")
+        else
+            existing_cxx_flags = []
+        end
+        extra_cxx_flags  = Array.new
+
+        deb_cxx_flags.each do |flag|
+            if existing_cxx_flags.include?(flag)
+                existing_cxx_flags.delete(flag)
+            end
+        end
+        cxx_flags = deb_cxx_flags + existing_cxx_flags
+
+        cxx_flags = cxx_flags.join(" ")
+        if !cxx_flags.empty?
+            pkg.define "CMAKE_CXX_FLAGS", cxx_flags
+        end
+    end
+end
