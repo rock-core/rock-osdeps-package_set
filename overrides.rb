5dcff12a2f0145381db5d1023cef7a7de1ba57df
Thomas Roehr <thomas.roehr@dfki.de>
2017-07-04 18:50:09 +0200

    Fix checking for typelib_plugin_path

--- overrides.rb
+++ overrides.rb
@@ -9,7 +9,7 @@ if Autoproj.user_config('DEB_USE')
 
     # Make sure the release path to typelib is used, when there is no local
     # installation
-    if Dir.glob(File.join(typelib_plugin_path, "**")).empty?
+    if !typelib_plugin_path || Dir.glob(File.join(typelib_plugin_path, "**")).empty?
         release_dir="/opt/rock/#{Autoproj.user_config('debian_release')}"
         Autoproj.env_set 'TYPELIB_PLUGIN_PATH', File.join(release_dir, 'lib','typelib')
     end
