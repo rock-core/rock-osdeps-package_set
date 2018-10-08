911156ef61c8fd9002c2a1e3f1e92fcce83be0f8
Thomas Roehr <thomas.roehr@dfki.de>
2018-10-08 11:24:05 +0200

    Fix tests

--- test/test.rb
+++ test/test.rb
@@ -6,15 +6,15 @@ require_relative '../lib/package_selector'
 module Rock
     describe "setup_rock_osdeps" do
         before do
-            file = File.join(File.dirname(__FILE__),"..","data","master-17.06-amd64.yml")
+            file = File.join(File.dirname(__FILE__),"..","data","master-18.01-amd64.yml")
             @ps = Rock::DebianPackaging::PackageSelector.new
             @ps.load_osdeps_file(file)
         end
 
         describe "#initialize" do
             it "initializes the internal structures" do
-                assert @ps.pkg_to_deb["base/types"] = "rock-master-17.06-base-types"
-                assert @ps.deb_to_pkg["rock-master-17.06-base-types"] = "base/types"
+                assert @ps.pkg_to_deb["base/types"] = "rock-master-18.01-base-types"
+                assert @ps.deb_to_pkg["rock-master-18.01-base-types"] = "base/types"
             end
 
             it "computes reverse dependencies" do
